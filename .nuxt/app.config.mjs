e2e48866001acd1d732b99240ce6bf2d8f0616b0
Diash12 <156783724+Diash12@users.noreply.github.com>
2024-02-06 17:49:24 +0100

    fixed all problems

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/enctours/Documents/GitHub/ENC-TOURS1/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/enctours/Documents/GitHub/ENC-TOURS1/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
