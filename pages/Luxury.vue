f78490fddd826ea42b97ebd1c49eea4da2f2f8b0
Diash12 <156783724+Diash12@users.noreply.github.com>
2024-02-29 12:21:07 +0100

    made Luxury page adaptive

--- pages/Luxury.vue
+++ pages/Luxury.vue
@@ -3,10 +3,10 @@
     <div>
       <div class="relative z-0 w-full parallax">
         <div
-          class="w-1/2 m-auto pt-80 text-center z-10 uppercase text-white p-2"
+          class="w-1/2 m-auto mainDiv pt-80 text-center z-10 uppercase text-white p-2"
         >
-          <p class="text-7xl uppercase font-semibold">ENC LUXURy</p>
-          <p class="rounded-md mt-5 text-center">
+          <p class="text2 text-7xl uppercase font-semibold">ENC LUXURy</p>
+          <p class="rounded-md mt-5 text-center textP">
             Lauded for its extensive knowledge, its East-meets-West model and
             venturing responsibly into every corner of its destinations, it is
             our mission to show guests the beating heart of our Asia
@@ -34,7 +34,7 @@
               mass: 5,
             },
           }"
-          class="border w-1/2 m-auto p-8 text-customGray"
+          class="border introText w-1/2 m-auto p-8 text-customGray"
         >
           <p>
             ENC Tours provides captivating combined tours that seamlessly unite
@@ -51,171 +51,214 @@
             destinations.
           </p>
         </div>
-        <div
-        class="flex w-10/12 m-auto mt-10 uppercase mb-2"
-          v-motion
-          :initial="{
-            opacity: 0,
-            y: 100,
-          }"
-          :visible="{
-            opacity: 1,
-            y: 0,
-            transition: {
-              type: 'spring',
-              stiffness: 250,
-              damping: 70,
-              mass: 20,
-            },
-          }"
-        >
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
-              <img
-                src="@/assets/images/yact.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
-                alt=""
-                @mouseover="addBorder1"
-                @mouseout="removeBorder1"
-              />
-            </div>
-            <div class="pl-8 text-left">
-              <p
-                ref="section1"
-                class="pb-1"
-                @mouseover="addBorder1"
-                @mouseout="removeBorder1"
-              >
-                PRIVATE CASPIAN CRUISE
-              </p>
-            </div>
+        <div class="mainSection flex w-10/12 m-auto mt-10 uppercase mb-2">
+          <div
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 sectionDiv uppercase"
+          >
+            <img
+              src="@/assets/images/yact.jpeg"
+              class="imageStyle mb-3 h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              alt=""
+              @mouseover="addBorder1"
+              @mouseout="removeBorder1"
+            />
+            <p
+              ref="section1"
+              class="pb-1"
+              @mouseover="addBorder1"
+              @mouseout="removeBorder1"
+            >
+              PRIVATE CASPIAN CRUISE
+            </p>
           </div>
 
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
-              <img
-                src="@/assets/images/helic.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
-                alt=""
-                @mouseover="addBorder2"
-                @mouseout="removeBorder2"
-              />
-            </div>
-            <div class="pl-8 text-left">
-              <p
-                ref="section2"
-                class="pb-1"
-                @mouseover="addBorder2"
-                @mouseout="removeBorder2"
-              >
-                HELICOPTER AND PRIVATE JET TOURS
-              </p>
-            </div>
+          <div
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv"
+          >
+            <img
+              src="@/assets/images/helic.jpeg"
+              class="imageStyle mb-3 h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              alt=""
+              @mouseover="addBorder2"
+              @mouseout="removeBorder2"
+            />
+            <p
+              ref="section2"
+              class="pb-1"
+              @mouseover="addBorder2"
+              @mouseout="removeBorder2"
+            >
+              HELICOPTER AND PRIVATE JET TOURS
+            </p>
           </div>
 
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
-              <img
-                src="@/assets/images/retrocars.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
-                alt=""
-                @mouseover="addBorder3"
-                @mouseout="removeBorder3"
-              />
-            </div>
-            <div class="pl-8 text-left">
-              <p
-                ref="section3"
-                class="pb-1"
-                @mouseover="addBorder3"
-                @mouseout="removeBorder3"
-              >
-                RETRO CARS RALLY
-              </p>
-            </div>
+          <div
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 sectionDiv uppercase"
+          >
+            <img
+              src="@/assets/images/retrocars.jpeg"
+              class="imageStyle mb-3 h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              alt=""
+              @mouseover="addBorder3"
+              @mouseout="removeBorder3"
+            />
+            <p
+              ref="section3"
+              class="pb-1"
+              @mouseover="addBorder3"
+              @mouseout="removeBorder3"
+            >
+              RETRO CARS RALLY
+            </p>
           </div>
         </div>
-        <div
-          class="flex w-10/12 m-auto mt-10 mb-8"
-          v-motion
-          :initial="{
-            opacity: 0,
-            y: 100,
-          }"
-          :visible="{
-            opacity: 1,
-            y: 0,
-            transition: {
-              type: 'spring',
-              stiffness: 250,
-              damping: 70,
-              mass: 20,
-            },
-          }"
-        >
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
-              <img
-                src="@/assets/images/caviar1.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
-                alt=""
-                @mouseover="addBorder4"
-                @mouseout="removeBorder4"
-              />
-            </div>
-            <div class="pl-8 text-left">
-              <p
-                ref="section4"
-                class="pb-1"
-                @mouseover="addBorder4"
-                @mouseout="removeBorder4"
-              >
-                BLACK CAVIAR DEGUSTATION
-              </p>
-            </div>
+        <div class="mainSection flex justify-between w-10/12 m-auto mt-10 mb-8">
+          <div
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv"
+          >
+            <img
+              src="@/assets/images/caviar1.jpeg"
+              class="imageStyle mb-3 h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              alt=""
+              @mouseover="addBorder4"
+              @mouseout="removeBorder4"
+            />
+            <p
+              ref="section4"
+              class="pb-1"
+              @mouseover="addBorder4"
+              @mouseout="removeBorder4"
+            >
+              BLACK CAVIAR DEGUSTATION
+            </p>
           </div>
-
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
-              <img
-                src="@/assets/images/lux4.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
-                alt=""
-                @mouseover="addBorder5"
-                @mouseout="removeBorder5"
-              />
-            </div>
-            <div class="pl-8 text-left">
-              <p
-                ref="section5"
-                class="pb-1"
-                @mouseover="addBorder5"
-                @mouseout="removeBorder5"
-              >
-                EXCLUSIVE VENUES AND EVENTS
-              </p>
-            </div>
+          <div
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 sectionDiv uppercase"
+          >
+            <img
+              src="@/assets/images/lux4.jpeg"
+              class="imageStyle mb-3 h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              alt=""
+              @mouseover="addBorder5"
+              @mouseout="removeBorder5"
+            />
+            <p
+              ref="section5"
+              class="pb-1"
+              @mouseover="addBorder5"
+              @mouseout="removeBorder5"
+            >
+              EXCLUSIVE VENUES AND EVENTS
+            </p>
           </div>
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
-              <img
-                src="@/assets/images/golf.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
-                alt=""
-                @mouseover="addBorder6"
-                @mouseout="removeBorder6"
-              />
-            </div>
-            <div class="pl-8 text-left">
-              <p
-                ref="section6"
-                class="pb-1"
-                @mouseover="addBorder6"
-                @mouseout="removeBorder6"
-              >
-                GOLF TOUR
-              </p>
-            </div>
+          <div
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 sectionDiv uppercase"
+          >
+            <img
+              src="@/assets/images/golf.jpeg"
+              class="imageStyle mb-3 h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              alt="golf"
+              @mouseover="addBorder6"
+              @mouseout="removeBorder6"
+            />
+            <p
+              ref="section6"
+              class="pb-1"
+              @mouseover="addBorder6"
+              @mouseout="removeBorder6"
+            >
+              GOLF TOUR
+            </p>
           </div>
         </div>
       </div>
@@ -275,5 +318,57 @@ const { addBorder: addBorder6, removeBorder: removeBorder6 } =
   background-attachment: fixed;
   filter: brightness(55%);
   z-index: -1;
+}
+
+.text2 {
+  font-weight: 600;
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: x-large;
+  }
+}
+.introText {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 90%;
+    font-size: small;
+  }
+}
+.textP {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: small;
+    margin-top: 40px;
+  }
+}
+.mainDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    padding-top: 220px;
+    width: 80%;
+  }
+}
+.mainSection {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: medium;
+    display: block;
+  }
+}
+.sectionDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 100%;
+    border-right: none;
+    margin-bottom: 15px;
+  }
+}
+.imageStyle {
+  @media (min-width: 320px) and (max-width: 425px) {
+    height: 160px;
+    margin-bottom: 10px;
+  }
+  @media (min-width: 426px) and (max-width: 768px) {
+    height: 300px;
+    margin-bottom: 10px;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    height: 180px;
+    margin-bottom: 10px;
+  }
 }
 </style>
