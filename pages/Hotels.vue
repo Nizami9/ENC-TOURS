b2530d6383073c2e5fb8dfa65304ccd326d2bdf3
Diash12 <156783724+Diash12@users.noreply.github.com>
2024-03-01 14:16:18 +0100

    made hotels page adaptive

--- pages/Hotels.vue
+++ pages/Hotels.vue
@@ -3,10 +3,10 @@
     <div>
       <div class="relative z-0 w-full parallax">
         <div
-          class="w-1/2 m-auto pt-80 text-center z-10 uppercase text-white p-2"
+          class="w-1/2 mainDiv m-auto pt-80 text-center z-10 uppercase text-white p-2"
         >
-          <p class="text-7xl uppercase font-semibold">Hotels</p>
-          <p class="rounded-md mt-5 text-center">
+          <p class="text2 text-7xl uppercase font-semibold">Hotels</p>
+          <p class="rounded-md mt-5 text-center textP">
             Lauded for its extensive knowledge, its East-meets-West model and
             venturing responsibly into every corner of its destinations, it is
             our mission to show guests the beating heart of our Asia
@@ -15,7 +15,7 @@
       </div>
       <div class="text-center">
         <div class="p-8">
-          <p class="text-6xl">INTRO</p>
+          <p class="text-6xl mainP">INTRO</p>
           <p class="border-b-2 pt-4 w-20 border-gray-400 m-auto"></p>
         </div>
         <div
@@ -34,7 +34,7 @@
               mass: 5,
             },
           }"
-          class="border w-1/2 m-auto p-8 text-customGray"
+          class="border introText w-1/2 m-auto p-8 text-customGray"
         >
           <p>
             ENC Tours specializes in Ethnographic Tours, providing a unique
@@ -49,41 +49,39 @@
             an enriching experience for travelers.
           </p>
         </div>
-        <div
-          class="flex w-10/12 m-auto mt-10 uppercase mb-2"
-          v-motion
-          :initial="{
-            opacity: 0,
-            y: 100,
-          }"
-          :visible="{
-            opacity: 1,
-            y: 0,
-            transition: {
-              type: 'spring',
-              stiffness: 250,
-              damping: 70,
-              mass: 5,
-            },
-          }"
-        >
+        <div class="mainSection flex w-10/12 m-auto mt-10 uppercase mb-2">
           <div
-            class="w-1/3 uppercase cursor-pointer"
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv cursor-pointer"
             @click="navigateTo('/baku/bakuhotels')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/az21.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
                 alt=""
                 @mouseover="addBorder1"
                 @mouseout="removeBorder1"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="pb-8">
               <p
                 ref="section1"
-                class="pb-1"
+                class="text-color pb-1"
                 @mouseover="addBorder1"
                 @mouseout="removeBorder1"
               >
@@ -92,22 +90,37 @@
             </div>
           </div>
           <div
-            class="w-1/3 uppercase cursor-pointer"
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv cursor-pointer"
             @click="navigateTo('/gabala/gabalahotels')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/gabala.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
                 alt=""
                 @mouseover="addBorder2"
                 @mouseout="removeBorder2"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="pb-8">
               <p
                 ref="section2"
-                class="pb-1"
+                class="text-color pb-1"
                 @mouseover="addBorder2"
                 @mouseout="removeBorder2"
               >
@@ -116,22 +129,37 @@
             </div>
           </div>
           <div
-            class="w-1/3 uppercase cursor-pointer"
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv cursor-pointer"
             @click="navigateTo('/sheki/shekihotels')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/sheki5.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
                 alt=""
                 @mouseover="addBorder3"
                 @mouseout="removeBorder3"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="pb-8">
               <p
                 ref="section3"
-                class="pb-1"
+                class="text-color pb-1"
                 @mouseover="addBorder3"
                 @mouseout="removeBorder3"
               >
@@ -140,41 +168,39 @@
             </div>
           </div>
         </div>
-        <div
-          class="flex w-10/12 m-auto mt-10 uppercase mb-2"
-          v-motion
-          :initial="{
-            opacity: 0,
-            y: 100,
-          }"
-          :visible="{
-            opacity: 1,
-            y: 0,
-            transition: {
-              type: 'spring',
-              stiffness: 250,
-              damping: 70,
-              mass: 5,
-            },
-          }"
-        >
+        <div class="mainSection flex w-10/12 m-auto mt-10 uppercase mb-2">
           <div
-            class="w-1/3 uppercase cursor-pointer"
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv cursor-pointer"
             @click="navigateTo('/ganja/ganjahotels')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/ganja.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
                 alt=""
                 @mouseover="addBorder4"
                 @mouseout="removeBorder4"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="pb-8">
               <p
                 ref="section4"
-                class="pb-1"
+                class="text-color pb-1"
                 @mouseover="addBorder4"
                 @mouseout="removeBorder4"
               >
@@ -183,22 +209,37 @@
             </div>
           </div>
           <div
-            class="w-1/3 uppercase cursor-pointer"
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv cursor-pointer"
             @click="navigateTo('/guba/gubahotels')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/guba.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
                 alt=""
                 @mouseover="addBorder5"
                 @mouseout="removeBorder5"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="pb-8">
               <p
                 ref="section5"
-                class="pb-1"
+                class="text-color pb-1"
                 @mouseover="addBorder5"
                 @mouseout="removeBorder5"
               >
@@ -207,22 +248,37 @@
             </div>
           </div>
           <div
-            class="w-1/3 uppercase cursor-pointer"
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+            class="w-1/3 uppercase sectionDiv cursor-pointer"
             @click="navigateTo('/lankaran/lankaranhotels')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/lankaran.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
                 alt=""
                 @mouseover="addBorder6"
                 @mouseout="removeBorder6"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="pb-8">
               <p
                 ref="section6"
-                class="pb-1"
+                class="text-color pb-1"
                 @mouseover="addBorder6"
                 @mouseout="removeBorder6"
               >
@@ -233,40 +289,40 @@
         </div>
       </div>
       <div
-        class="flex w-10/12 m-auto mt-10 uppercase mb-2"
-        v-motion
-        :initial="{
-          opacity: 0,
-          y: 100,
-        }"
-        :visible="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            type: 'spring',
-            stiffness: 250,
-            damping: 70,
-            mass: 5,
-          },
-        }"
+        class="mainSection text-center flex w-10/12 m-auto mt-10 uppercase mb-2"
       >
         <div
-          class="w-1/3 uppercase cursor-pointer"
+          v-motion
+          :initial="{
+            opacity: 0,
+            y: 100,
+          }"
+          :visible="{
+            opacity: 1,
+            y: 0,
+            transition: {
+              type: 'spring',
+              stiffness: 250,
+              damping: 70,
+              mass: 5,
+            },
+          }"
+          class="w-1/3 uppercase sectionDiv cursor-pointer"
           @click="navigateTo('/shahdag/shahdaghotels')"
         >
-          <div class="pl-8 pb-2">
+          <div class="pb-2">
             <img
               src="@/assets/images/shahdag.jpeg"
-              class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
+              class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all"
               alt=""
               @mouseover="addBorder7"
               @mouseout="removeBorder7"
             />
           </div>
-          <div class="pl-8 pb-8 text-left">
+          <div class="pb-8">
             <p
               ref="section7"
-              class="pb-1"
+              class="text-color pb-1"
               @mouseover="addBorder7"
               @mouseout="removeBorder7"
             >
@@ -334,5 +390,68 @@ const { addBorder: addBorder7, removeBorder: removeBorder7 } =
   background-attachment: fixed;
   filter: brightness(55%);
   z-index: -1;
+}
+
+.text-color {
+  color: #5a5858;
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 100%;
+    font-size: small;
+  }
+}
+.text2 {
+  font-weight: 600;
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: x-large;
+  }
+}
+.introText {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 90%;
+    font-size: small;
+  }
+}
+.textP {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: small;
+    margin-top: 40px;
+  }
+}
+.mainDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    padding-top: 220px;
+    width: 80%;
+  }
+}
+.mainSection {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: medium;
+    display: block;
+  }
+}
+.sectionDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 100%;
+    border-right: none;
+  }
+}
+.imageStyle {
+  @media (min-width: 320px) and (max-width: 374px) {
+    height: 160px;
+  }
+  @media (min-width: 375px) and (max-width: 425px) {
+    height: 190px;
+  }
+  @media (min-width: 426px) and (max-width: 768px) {
+    height: 320px;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    height: 180px;
+  }
+}
+.mainP {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: xx-large;
+  }
 }
 </style>
