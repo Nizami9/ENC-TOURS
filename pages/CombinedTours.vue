6a2dbf410ef91eabb24d3814f4d0e8dcb3f9f0e0
Diash12 <156783724+Diash12@users.noreply.github.com>
2024-03-01 13:23:00 +0100

    made adaptive design for Combined Tours page

--- pages/CombinedTours.vue
+++ pages/CombinedTours.vue
@@ -3,10 +3,10 @@
     <div>
       <div class="relative z-0 w-full parallax">
         <div
-          class="w-1/2 m-auto pt-80 text-center z-10 uppercase text-white p-2"
+          class="w-1/2 mainDiv m-auto pt-80 text-center z-10 uppercase text-white p-2"
         >
-          <p class="text-7xl uppercase font-semibold">COMBINED TOURS</p>
-          <p class="rounded-md mt-5 text-center">
+          <p class="text2 text-7xl uppercase font-semibold">COMBINED TOURS</p>
+          <p class="rounded-md mt-5 text-center textP">
             Lauded for its extensive knowledge, its East-meets-West model and
             venturing responsibly into every corner of its destinations, it is
             our mission to show guests the beating heart of our Asia
@@ -15,7 +15,7 @@
       </div>
       <div class="text-center">
         <div class="p-8">
-          <p class="text-6xl">INTRO</p>
+          <p class="text-6xl mainP">INTRO</p>
           <p class="border-b-2 pt-4 w-20 border-gray-400 m-auto"></p>
         </div>
         <div
@@ -34,7 +34,7 @@
               mass: 5,
             },
           }"
-          class="border w-1/2 m-auto p-8 text-customGray"
+          class="border introText w-1/2 m-auto p-8 text-customGray"
         >
           <p>
             ENC Tours provides captivating combined tours that seamlessly unite
@@ -52,7 +52,7 @@
           </p>
         </div>
         <div
-          class="flex w-10/12 m-auto mt-10 uppercase mb-2"
+          class="flex mainSection w-10/12 m-auto mt-10 uppercase mb-2"
           v-motion
           :initial="{
             opacity: 0,
@@ -69,17 +69,17 @@
             },
           }"
         >
-          <div class="w-1/3 uppercase">
-            <div class="pl-8 pb-2">
+          <div class="w-1/3 sectionDiv uppercase">
+            <div class="pb-2">
               <img
                 src="@/assets/images/baku.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all cursor-pointer"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all cursor-pointer"
                 alt=""
                 @mouseover="addBorder1"
                 @mouseout="removeBorder1"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="text-color pb-8">
               <p
                 ref="section1"
                 class="pb-1"
@@ -91,19 +91,19 @@
             </div>
           </div>
           <div
-            class="w-1/3 uppercase"
+            class="w-1/3 sectionDiv uppercase"
             @click="navigateTo('../tours/Explore_Caucasus')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/baku2.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all cursor-pointer"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all cursor-pointer"
                 alt=""
                 @mouseover="addBorder2"
                 @mouseout="removeBorder2"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="text-color pb-8">
               <p
                 ref="section2"
                 class="pb-1"
@@ -115,19 +115,19 @@
             </div>
           </div>
           <div
-            class="w-1/3 uppercase"
+            class="w-1/3 sectionDiv uppercase"
             @click="navigateTo('../tours/Azerbaijan_Georgia')"
           >
-            <div class="pl-8 pb-2">
+            <div class="pb-2">
               <img
                 src="@/assets/images/georg.jpeg"
-                class="h-60 w-full border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all cursor-pointer"
+                class="imageStyle h-60 w-11/12 m-auto border-2 rounded-md border-gray-400 hover:scale-95 duration-300 transition-all cursor-pointer"
                 alt=""
                 @mouseover="addBorder3"
                 @mouseout="removeBorder3"
               />
             </div>
-            <div class="pl-8 pb-8 text-left">
+            <div class="text-color pb-8">
               <p
                 ref="section3"
                 class="pb-1"
@@ -189,5 +189,66 @@ const { addBorder: addBorder3, removeBorder: removeBorder3 } =
   background-attachment: fixed;
   filter: brightness(55%);
   z-index: -1;
+}
+
+.text-color {
+  color: #5a5858;
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 100%;
+    font-size: small;
+  }
+}
+
+.text2 {
+  font-weight: 600;
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: x-large;
+  }
+}
+.introText {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 90%;
+    font-size: small;
+  }
+}
+.textP {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: small;
+    margin-top: 40px;
+  }
+}
+.mainDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    padding-top: 220px;
+    width: 80%;
+  }
+}
+.mainSection {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: medium;
+    display: block;
+  }
+}
+.sectionDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 100%;
+    border-right: none;
+  }
+}
+.imageStyle {
+  @media (min-width: 320px) and (max-width: 425px) {
+    height: 160px;
+  }
+  @media (min-width: 426px) and (max-width: 768px) {
+    height: 300px;
+  }
+  @media (min-width: 769px) and (max-width: 1024px) {
+    height: 180px;
+  }
+}
+.mainP {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: xx-large;
+  }
 }
 </style>
