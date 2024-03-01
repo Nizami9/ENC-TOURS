4aa7f1d8865b5a95713fa4781e8ac8153564f91e
Diash12 <156783724+Diash12@users.noreply.github.com>
2024-03-01 16:08:28 +0100

    made ContactUS page adaptive

--- pages/contactUs.vue
+++ pages/contactUs.vue
@@ -3,34 +3,36 @@
     <div>
       <div class="relative z-0 w-full parallax">
         <div
-          class="w-1/3 m-auto pt-80 text-center z-10 uppercase text-white p-2"
+          class="w-1/3 mainDiv m-auto pt-80 text-center z-10 uppercase text-white p-2"
         >
-          <p class="text-7xl uppercase font-semibold">contact us</p>
+          <p class="text2 text-7xl uppercase font-semibold">contact us</p>
         </div>
       </div>
       <div class="text-center">
         <div class="p-8">
-          <p class="font-semibold text-xl">
+          <p class="font-semibold text-xl mainP">
             WE’RE ALWAYS EAGER TO HEAR FROM YOU.
           </p>
-          <p class="border-b-2 pt-4 w-20 border-orange-500 m-auto"></p>
+          <p class="border-b-2 pt-4 w-20 border-gray-500 m-auto"></p>
         </div>
-        <div class="border w-1/2 m-auto p-8 text-customGray">
-          <p  v-motion
-      :initial="{
-        opacity: 0,
-        y: 100,
-      }"
-      :visible="{
-        opacity: 1,
-        y: 0,
-        transition: {
-          type: 'spring',
-          stiffness: 250,
-          damping: 70,
-          mass: 5,
-        },
-      }">
+        <div class="border introText w-1/2 m-auto p-8 text-customGray">
+          <p
+            v-motion
+            :initial="{
+              opacity: 0,
+              y: 100,
+            }"
+            :visible="{
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: 'spring',
+                stiffness: 250,
+                damping: 70,
+                mass: 5,
+              },
+            }"
+          >
             For questions, assistance or requests for collaboration, Asian
             Trails is committed to reply to requests within the shortest time
             possible during office hours in each respective time zone. We’re
@@ -39,87 +41,33 @@
             sales offices.
           </p>
         </div>
-        <div class="p-8 m-auto w-full">
+        <div class="introText pt-8 pb-8">
           <p class="font-semibold text-xl mb-4">ENC TOURS - HEAD OFFICE</p>
-          <div class="flex m-auto w-1/4">
+          <div class="flex eachSec justify-between w-1/5 m-auto text-center">
             <img
               src="@/assets/images/home.png"
-              class="w-6 mr-8 mb-2"
+              class="h-6 mb-2"
               alt="homeIcon"
             />
-            <p
-              v-motion
-              :initial="{
-                opacity: 0,
-                y: 100,
-              }"
-              :visible="{
-                opacity: 1,
-                y: 0,
-                transition: {
-                  type: 'spring',
-                  stiffness: 250,
-                  damping: 70,
-                  mass: 5,
-                },
-              }"
-            >
-              4th floor, 47 Füzuli Küçəsi, Bakı 1014, Azerbaijan
-            </p>
+            <p class="text-center w-full">4th floor, 47 Füzuli Küçəsi, Bakı 1014, Azerbaijan</p>
           </div>
-          <div class="flex m-auto w-1/4">
+          <div class="flex eachSec justify-between w-1/5 m-auto text-center">
             <img
               src="@/assets/images/mail.png"
-              class="w-6 mr-8 mb-2"
+              class="h-6 mb-2"
               alt="homeIcon"
             />
-            <p
-              v-motion
-              :initial="{
-                opacity: 0,
-                y: 100,
-              }"
-              :visible="{
-                opacity: 1,
-                y: 0,
-                transition: {
-                  type: 'spring',
-                  stiffness: 250,
-                  damping: 70,
-                  mass: 5,
-                },
-              }"
-            >
-              info@enctours.az
-            </p>
+            <p class="text-center w-full">info@enctours.az</p>
           </div>
-          <div class="flex m-auto w-1/4">
+          <div class="flex eachSec justify-between w-1/5 m-auto text-center">
             <img
               src="@/assets/images/phone.png"
-              class="w-6 mr-8"
+              class="h-6"
               alt="phoneIcon"
             />
-            <p
-              v-motion
-              :initial="{
-                opacity: 0,
-                y: 100,
-              }"
-              :visible="{
-                opacity: 1,
-                y: 0,
-                transition: {
-                  type: 'spring',
-                  stiffness: 250,
-                  damping: 70,
-                  mass: 5,
-                },
-              }"
-            >
-              +994124974020
-            </p>
+            <p class="text-center w-full">+994124974020</p>
           </div>
-          <p class="border-b-4 pt-4 w-20 border-orange-500 m-auto"></p>
+          <p class="border-b-4 pt-4 w-40 border-gray-500 m-auto"></p>
         </div>
         <div>
           <iframe
@@ -141,8 +89,11 @@
 
 <style scoped>
 .parallax {
-  position: relative;
+  background-image: url("@/assets/images/newAz1.jpeg");
   min-height: 100vh;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 .parallax::before {
   content: "";
@@ -157,5 +108,34 @@
   background-attachment: fixed;
   filter: brightness(55%);
   z-index: -1;
+}
+.text2 {
+  font-weight: 600;
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: x-large;
+  }
+}
+.introText {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 90%;
+    font-size: small;
+    margin: auto;
+  }
+}
+.mainDiv {
+  @media (min-width: 320px) and (max-width: 820px) {
+    padding-top: 220px;
+    width: 80%;
+  }
+}
+.eachSec {
+  @media (min-width: 320px) and (max-width: 820px) {
+    width: 60%;
+  }
+}
+.mainP {
+  @media (min-width: 320px) and (max-width: 820px) {
+    font-size: xx-large;
+  }
 }
 </style>
