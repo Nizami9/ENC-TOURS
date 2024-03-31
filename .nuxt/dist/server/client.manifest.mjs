709ab1fc6741e052f6ab1d4904c0a5f859148dd3
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2024-03-31 12:28:43 +0200

    Update client.manifest.mjs

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,15 +1,1 @@
-export default {
-  "@vite/client": {
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "/Users/aaliyyah/Documents/GitHub/ENC-TOURS/node_modules/nuxt/dist/app/entry.mjs": {
-    "resourceType": "script",
-    "module": true,
-    "isEntry": true,
-    "file": "/Users/aaliyyah/Documents/GitHub/ENC-TOURS/node_modules/nuxt/dist/app/entry.mjs"
-  }
-}
+export { default } from "file:///Users/aaliyyah/Documents/GitHub/ENC-TOURS/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
